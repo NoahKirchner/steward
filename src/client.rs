9084c3c29ddde05a0e9ad07fbc2a89974b8b428c
Noah <noahthomkirchner@gmail.com>
2024-02-05 10:13:40 -0500

    Does a bunch of stupid stuff like allowing retries on 500 internal server errors and adding a clone timeout

--- src/client.rs
+++ src/client.rs
@@ -1,22 +1,51 @@
 use crate::auth::*;
-use std::{error::Error, collections::HashMap};
-use reqwest::{ClientBuilder, header::{HeaderMap, AUTHORIZATION, HeaderValue}};
-use serde_json::{Value};
+use reqwest::{
+    header::{HeaderMap, HeaderValue, AUTHORIZATION},
+    Client, Request, Response,
+};
+use reqwest_middleware::{ClientBuilder, Middleware, Next};
+use reqwest_retry::{
+    default_on_request_failure, policies::ExponentialBackoff, RetryTransientMiddleware, Retryable,
+    RetryableStrategy,
+};
+use serde_json::Value;
+use std::{collections::HashMap, error::Error};
 
-pub async fn build_client()->Result<StewardClient, Box<dyn Error>>{
+struct RetryStrategy;
+impl RetryableStrategy for RetryStrategy {
+    fn handle(&self, res: &reqwest_middleware::Result<reqwest::Response>) -> Option<Retryable> {
+        match res {
+            Ok(success) if success.status() != 200 => Some(Retryable::Transient),
+            Ok(success) => None,
+            Err(error) => default_on_request_failure(error),
+        }
+    }
+}
+
+pub async fn build_client() -> Result<StewardClient, Box<dyn Error>> {
     let auth_data = get_auth_variables()?;
     let mut headers = HeaderMap::new();
     let url = auth_data.address;
     let key = auth_data.key.as_str();
-    
-    let client = ClientBuilder::new()
+
+    let retry_policy = ExponentialBackoff::builder().build_with_max_retries(5);
+
+    let raw_client = reqwest::ClientBuilder::new()
         .danger_accept_invalid_certs(true)
         .build()?;
 
+    // This ClientBuilder uses the reqwest_middleware builder instead.
+    let client = ClientBuilder::new(raw_client)
+        .with(RetryTransientMiddleware::new_with_policy_and_strategy(
+            retry_policy,
+            RetryStrategy,
+        ))
+        .build();
+
     headers.insert(AUTHORIZATION, HeaderValue::from_str(key)?);
-    
+
     let config_url = format!("{}/api2/json/cluster/status", url.clone());
-    let config = client 
+    let config = client
         .get(config_url.clone())
         .headers(headers.clone())
         .send()
@@ -28,47 +57,46 @@ pub async fn build_client()->Result<StewardClient, Box<dyn Error>>{
             let v: Value = serde_json::from_str(&config_data)?;
             // I know this looks horrible, but it gets rid of the quotes around the cluster name.
             let cluster_name = v["data"][0]["name"].to_string().replace("\"", "");
-            Ok(
-                StewardClient {
-                    client,
-                    url,
-                    headers,
-                    cluster_name,
-                    current_node: None,
-                }
-                )
+            Ok(StewardClient {
+                client,
+                url,
+                headers,
+                cluster_name,
+                current_node: None,
+            })
         }
         // Replace this god awfulness
-        _ => { panic!("FIX ME PLERASE" )}
+        _ => {
+            panic!("FIX ME PLERASE")
+        }
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct StewardClient {
-    client: reqwest::Client,
+    client: reqwest_middleware::ClientWithMiddleware,
     url: String,
     headers: HeaderMap,
     pub cluster_name: String,
     pub current_node: Option<String>,
-
 }
 
 impl StewardClient {
-
-    pub fn set_node(&mut self, node:String)->Result<(), Box<dyn Error>> {
+    pub fn set_node(&mut self, node: String) -> Result<(), Box<dyn Error>> {
         self.current_node = Some(node);
         Ok(())
     }
 
     /*
      * This is sort of ugly, but basically when trying to get cluster/node information, proxmox
-     * will return the "data" object that contains an array of objects. All this does is inspect 
+     * will return the "data" object that contains an array of objects. All this does is inspect
      * each object in the array, extract the name field from the object, and turn its data into a
      * hashmap, and then throws THAT into another hashmap (I know, it's horrible, forgive me) with
      * the key equal to the name of the node.
      */
-    pub async fn about(&self)->Result<HashMap<String, HashMap<String, Value>>, Box<dyn Error>>{
-        let about = self.client
+    pub async fn about(&self) -> Result<HashMap<String, HashMap<String, Value>>, Box<dyn Error>> {
+        let about = self
+            .client
             .get(format!("{}/api2/json/cluster/status", self.url))
             //TODO Try to kill this .clone but it might not be possible
             .headers(self.headers.clone())
@@ -80,43 +108,62 @@ impl StewardClient {
         let v: Value = serde_json::from_str(about.as_str()).unwrap();
         let node_list: Vec<Value> = serde_json::from_value(v["data"].clone())?;
 
-
         // Lord forgive me for the sin of a nested hashmaps but structs would be even more stupid
 
         let mut node_map: HashMap<String, HashMap<String, Value>> = HashMap::new();
         for entry in node_list.into_iter() {
             let mut node: HashMap<String, Value> = serde_json::from_value(entry.clone()).unwrap();
-            
+
             if node.get("name").is_some() {
-                node_map.insert(node.remove("name").unwrap().as_str().unwrap().to_string(), node);
+                node_map.insert(
+                    node.remove("name").unwrap().as_str().unwrap().to_string(),
+                    node,
+                );
             }
         }
 
         Ok(node_map)
     }
 
-    pub async fn clone_vm(&self, node:String, source_vmid:i32, clone_args:HashMap<&str, Value>)->Result<(), Box<dyn Error>> {
-
+    pub async fn clone_vm(
+        &self,
+        node: String,
+        source_vmid: i32,
+        clone_args: HashMap<&str, Value>,
+    ) -> Result<(), Box<dyn Error>> {
         // TODO Check here to see if a pool exists or if a vmid is conflicting with the destination
         // otherwise the clone will fail
-        let clone = self.client 
-            .post(format!("{}/api2/json/nodes/{node}/qemu/{source_vmid}/clone", self.url))
+        //
+
+        // REALLY BAD SOLUTION BUT we are on a time crunch my brutha
+        let timeout = std::time::Duration::from_millis(1000);
+        std::thread::sleep(timeout);
+        let clone = self
+            .client
+            .post(format!(
+                "{}/api2/json/nodes/{node}/qemu/{source_vmid}/clone",
+                self.url
+            ))
             .headers(self.headers.clone())
             .json(&clone_args)
             .send()
             .await?;
-            //.text()
-            //.await?;
+        //.text()
+        //.await?;
 
         dbg!(clone);
-
 
         Ok(())
     }
 
-    pub async fn destroy_vm(&self, node:String, vmid:i32, destroy_args:HashMap<&str, Value>)->Result<(), Box<dyn Error>> {
-        
-        let destroy = self.client 
+    pub async fn destroy_vm(
+        &self,
+        node: String,
+        vmid: i32,
+        destroy_args: HashMap<&str, Value>,
+    ) -> Result<(), Box<dyn Error>> {
+        let destroy = self
+            .client
             .delete(format!("{}/api2/json/nodes/{node}/qemu/{vmid}", self.url))
             .headers(self.headers.clone())
             // TODO figure out why sending arguments breaks vm destruction with 501 error
@@ -131,9 +178,13 @@ impl StewardClient {
         Ok(())
     }
 
-    pub async fn vm_status(&self, node:String, vmid:i32)->Result<(), Box<dyn Error>> {
-        let status = self.client 
-            .get(format!("{}/api2/json/nodes/{node}/qemu/{vmid}/status/current", self.url))
+    pub async fn vm_status(&self, node: String, vmid: i32) -> Result<(), Box<dyn Error>> {
+        let status = self
+            .client
+            .get(format!(
+                "{}/api2/json/nodes/{node}/qemu/{vmid}/status/current",
+                self.url
+            ))
             .headers(self.headers.clone())
             .send()
             .await?
@@ -144,5 +195,4 @@ impl StewardClient {
 
         Ok(())
     }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,25 @@ mod auth;
 use auth::*;
 mod client;
 use client::*;
-use std::{process::exit, collections::HashMap};
-use tokio;
-use std::error::Error;
+use core::time::Duration;
 use serde_json::Value;
-
+use std::error::Error;
+use std::thread;
+use std::{collections::HashMap, process::exit};
+use tokio;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    /*
+     * Until I find a better solution we are just going to do a timeout
+     */
 
     let client_error = "You do not appear to have connected to a proxmox cluster. Please run 'connect' before you use this command. Thanks!";
 
     let mut rl = Repl::<Cli>::new();
     let mut client: Option<StewardClient> = Option::None;
     loop {
+        let mut prompt: String = "STEWARD > ".to_string();
 
-
-        let mut prompt:String = "STEWARD > ".to_string();
-        
         // Checks the existence of the client for each loop iteration
         match &client {
             Some(x) => {
@@ -30,17 +32,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
             None => (),
         }
 
-
         let Some(command) = rl.read_command(prompt) else {
             continue;
         };
 
-
         match command.command {
-
-            ReplCommand::Connect { address, username, password } =>
-            {
-
+            ReplCommand::Connect {
+                address,
+                username,
+                password,
+            } => {
                 // Defined in auth.rs
                 set_auth_variables(address, username, password).await?;
 
@@ -50,178 +51,208 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         client = Some(x);
                     }
                     // TODO error handling
-                    Err(_) => todo!()
+                    Err(_) => todo!(),
                 }
             }
 
-
-        ReplCommand::Disconnect { test } => {
-            println!("{test} ");
-        }
-
-
-        ReplCommand::About { } => {
-
-            match &client {
-                Some(x) => {
-                    let output = &x.about().await?;
-                    //TODO pretty printing
-                    dbg!(output);
-                }
-                None => {
-                    println!("{}", client_error)
-                }
+            ReplCommand::Disconnect { test } => {
+                println!("{test} ");
             }
 
-
-        }
-
-        // This command only seems to have issues in bulk mode at massive numbers with a few 
-        // 500 internal server errors sneaking through. Handling this is going to be part of the
-        // job/task system, but just noting it down here for future reference. TODO 
-        ReplCommand::Clone { action, start_vmid, batches, node, source_vmid, dest_vmid, description, full, name, pool } => {
-            let mut clone_args = HashMap::new();
- 
-            if full.is_some() { clone_args.insert("full", Value::from(full)); }
-            if description.is_some() {clone_args.insert("description", Value::from(description)); }
-            if pool.is_some() { clone_args.insert("pool", Value::from(pool)); } 
-           
-            // Verifies that a client exists
-            match &client { 
-                Some(_client) => {
-                match action {
-
-                    // Checks if it was a bulk clone action.
-                    Some(CloneAction::Bulk) => {
-
-                        for vmid in start_vmid.unwrap() .. dest_vmid+1 {
-
-                            // Creates a copy of the function arguments
-                            let mut _clone_args = clone_args.clone();
-                            _clone_args.insert("newid", Value::from(vmid));
-                            // Just formats a name if it exists (TODO maybe just make this an if
-                            // statement)
-                            match &name {
-                                Some(_name) => {
-                                    _clone_args.insert("name", Value::from(format!("{}-{}", &_name, vmid)));
-                                }
-                                None => {
-                                    ();
-                                }     
-
-                            }
-
-                            let _output = &_client.clone_vm(node.to_owned(), source_vmid.to_owned(), _clone_args).await?;
-                        }
-                     
+            ReplCommand::About {} => {
+                match &client {
+                    Some(x) => {
+                        let output = &x.about().await?;
+                        //TODO pretty printing
+                        dbg!(output);
                     }
-                    // Checks if it was a batch clone action
-                    Some(CloneAction::Batch) => {
+                    None => {
+                        println!("{}", client_error)
+                    }
+                }
+            }
 
-                        // Returns a tuple of the padding size for the batch number (so you have
-                        // 000-250 instead of 0-250 for VMID purposes)
-                        let (padding_size, batches) = match batches {
-                            Some(_batches) => {
-                                (_batches.clone().to_string().len(), _batches)
+            // This command only seems to have issues in bulk mode at massive numbers with a few
+            // 500 internal server errors sneaking through. Handling this is going to be part of the
+            // job/task system, but just noting it down here for future reference. TODO
+            ReplCommand::Clone {
+                action,
+                start_vmid,
+                batches,
+                node,
+                source_vmid,
+                dest_vmid,
+                description,
+                full,
+                name,
+                pool,
+            } => {
+                let mut clone_args = HashMap::new();
+
+                if full.is_some() {
+                    clone_args.insert("full", Value::from(full));
+                }
+                if description.is_some() {
+                    clone_args.insert("description", Value::from(description));
+                }
+                if pool.is_some() {
+                    clone_args.insert("pool", Value::from(pool));
+                }
+
+                // Verifies that a client exists
+                match &client {
+                    Some(_client) => {
+                        match action {
+                            // Checks if it was a bulk clone action.
+                            Some(CloneAction::Bulk) => {
+                                for vmid in start_vmid.unwrap()..dest_vmid + 1 {
+                                    // Creates a copy of the function arguments
+                                    let mut _clone_args = clone_args.clone();
+                                    _clone_args.insert("newid", Value::from(vmid));
+                                    // Just formats a name if it exists (TODO maybe just make this an if
+                                    // statement)
+                                    match &name {
+                                        Some(_name) => {
+                                            _clone_args.insert(
+                                                "name",
+                                                Value::from(format!("{}-{}", &_name, vmid)),
+                                            );
+                                        }
+                                        None => {
+                                            ();
+                                        }
+                                    }
+
+                                    let _output = &_client
+                                        .clone_vm(
+                                            node.to_owned(),
+                                            source_vmid.to_owned(),
+                                            _clone_args,
+                                        )
+                                        .await?;
+                                }
+                            }
+                            // Checks if it was a batch clone action
+                            Some(CloneAction::Batch) => {
+                                // Returns a tuple of the padding size for the batch number (so you have
+                                // 000-250 instead of 0-250 for VMID purposes)
+                                let (padding_size, batches) = match batches {
+                                    Some(_batches) => {
+                                        (_batches.clone().to_string().len(), _batches)
+                                    }
+                                    None => {
+                                        panic!("Aw shit");
+                                    }
+                                };
+
+                                for batch in 0..batches + 1 {
+                                    // Formatting from the earlier comment to create a VMID with correct
+                                    // padding
+                                    let vmid = format!(
+                                        "{}{:0padding_size$}{}",
+                                        start_vmid.unwrap(),
+                                        batch,
+                                        dest_vmid,
+                                        padding_size = padding_size
+                                    )
+                                    .parse::<i32>()
+                                    .unwrap();
+
+                                    let mut _clone_args = clone_args.clone();
+                                    _clone_args.insert("newid", Value::from(vmid));
+
+                                    match &name {
+                                        Some(_name) => {
+                                            _clone_args.insert(
+                                                "name",
+                                                Value::from(format!("{}-{}", &_name, batch)),
+                                            );
+                                        }
+                                        None => {
+                                            ();
+                                        }
+                                    }
+
+                                    let _output = &_client
+                                        .clone_vm(
+                                            node.to_owned(),
+                                            source_vmid.to_owned(),
+                                            _clone_args,
+                                        )
+                                        .await?;
+                                }
+                            }
+                            // Triggers if this is just a normal single clone action
+                            None => {
+                                clone_args.insert("newid", Value::from(dest_vmid));
+                                if name.is_some() {
+                                    clone_args.insert("name", Value::from(name));
+                                }
+                                let _output =
+                                    &_client.clone_vm(node, source_vmid, clone_args).await?;
+                            }
+                        }
+                    }
+                    None => {
+                        println!("{}", client_error)
+                    }
+                }
+            }
+
+            ReplCommand::Destroy {
+                bulk,
+                node,
+                dest_vmid,
+                destroy_disks,
+                purge_jobs,
+            } => {
+                let mut destroy_args = HashMap::new();
+                if destroy_disks.is_some() {
+                    destroy_args.insert("destroy-unreferenced-disks", Value::from(destroy_disks));
+                }
+                if purge_jobs.is_some() {
+                    destroy_args.insert("purge", Value::from(purge_jobs));
+                }
+
+                if bulk.is_some() {
+                    for vmid in bulk.unwrap()..dest_vmid + 1 {
+                        let _output = match &client {
+                            Some(x) => {
+                                &x.destroy_vm(node.clone(), vmid, destroy_args.clone())
+                                    .await?;
                             }
                             None => {
-                                panic!("Aw shit");
+                                println!("{}", client_error);
                             }
                         };
-
-                        for batch in 0 .. batches+1 {
-                            // Formatting from the earlier comment to create a VMID with correct
-                            // padding
-                            let vmid = format!("{}{:0padding_size$}{}", 
-                                    start_vmid.unwrap(), 
-                                    batch,
-                                    dest_vmid,
-                                    padding_size = padding_size
-                                    ).parse::<i32>().unwrap();
-                            
-
-                            let mut _clone_args = clone_args.clone();
-                            _clone_args.insert("newid", Value::from(vmid));
-
-                            match &name {
-                                Some(_name) => {
-                                    _clone_args.insert("name", Value::from(format!("{}-{}", &_name, batch)));
-                                }
-                                None => {
-                                    ();
-                                }
-                            }
-
-                            let _output = &_client.clone_vm(node.to_owned(), source_vmid.to_owned(), _clone_args).await?;
-
-                        }
-    
                     }
-                    // Triggers if this is just a normal single clone action
+                }
+
+                let _output = match &client {
+                    Some(x) => {
+                        &x.destroy_vm(node, dest_vmid, destroy_args).await?;
+                    }
                     None => {
-
-                        clone_args.insert("newid", Value::from(dest_vmid));
-                        if name.is_some() { clone_args.insert("name", Value::from(name)); }
-                        let _output = &_client.clone_vm(node, source_vmid, clone_args).await?;
+                        println!("{}", client_error);
                     }
-                }
-
-                }
-                None => {
-                    println!("{}", client_error)
-                }
+                };
             }
 
-            
+            ReplCommand::Status { node, vmid } => {
+                let _output = match &client {
+                    Some(x) => {
+                        &x.vm_status(node, vmid).await?;
+                    }
+                    None => {
+                        println!("{}", client_error);
+                    }
+                };
             }
 
-        ReplCommand::Destroy { bulk, node, dest_vmid, destroy_disks, purge_jobs } => {
-            let mut destroy_args = HashMap::new();
-            if destroy_disks.is_some() {destroy_args.insert("destroy-unreferenced-disks", Value::from(destroy_disks)); }
-            if purge_jobs.is_some() {destroy_args.insert("purge", Value::from(purge_jobs)); }
-
-            if bulk.is_some() {
-                for vmid in bulk.unwrap() .. dest_vmid+1 {
-                    let _output = match &client { 
-                        Some(x) => {
-                            &x.destroy_vm(node.clone(), vmid, destroy_args.clone()).await?;
-                        }
-                        None => {
-                            println!("{}", client_error);
-                        }
-                    };
-
-                }
+            ReplCommand::Quit => {
+                println!("pce");
+                exit(0);
             }
-
-            let _output = match &client {
-                Some(x) => {
-                    &x.destroy_vm(node, dest_vmid, destroy_args).await?;
-                }
-                None => {
-                    println!("{}", client_error);
-                }
-            };
-        
-        }
-
-        ReplCommand::Status { node, vmid } => {
-            let _output = match &client {
-                Some(x) => {
-                    &x.vm_status(node, vmid).await?;
-                }
-                None => {
-                    println!("{}", client_error);
-                }
-            };
-        }
-
-        ReplCommand::Quit => {
-            println!("pce");
-            exit(0);
-        }
         }
     }
-
 }
-
